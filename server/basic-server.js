6f52f29fb239cce9b0fb8fa38180e0127dd82cf2
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-29 18:48:53 -0700

    add /classes/messages as a file to store all data

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,7 @@
 /* Import node's http module: */
 var http = require('http');
-const {defaultCorsHeaders, handleRequest} = require('./request-handler.js');
+const { defaultCorsHeaders, handleRequest } = require('./request-handler.js');
+const { messages } = require('./classes/messages.js');
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
